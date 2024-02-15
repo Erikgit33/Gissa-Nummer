b2928dd2d33b7069d60d28d6734213cd6c36f145
Erik Bäckström <erik.backstrom@skola.sala.se>
2024-02-15 14:39:18 +0100

    Added comments

--- Gissa Nummer/Form1.cs
+++ Gissa Nummer/Form1.cs
@@ -13,8 +13,8 @@ namespace Gissa_Nummer
     public partial class Form1 : Form
     {
         Random rng = new Random();
-        int number;
-        int guesses = 0;
+        int number; //One random variable which range is specified later, and two integrals. Number represents the value 
+        int guesses = 0; //that is randomized, and the other is for counting your guesses.
         
         public Form1()
         {
@@ -25,32 +25,35 @@ namespace Gissa_Nummer
         {
             string str = number.ToString();
             textBox1.Text = str;
-            
+            //You can cheat by pressing the label, the ranomized number will appear in the textbox.
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             int guess = int.Parse(textBox1.Text);
-           guesses = guesses + 1;
+            guesses = guesses + 1;
             if (guess == number)
             {
-                MessageBox.Show("Du gissade rätt efter " + guesses.ToString() + " Försök!");
+                MessageBox.Show("You guessed correctly after " + guesses.ToString() + " Guesses!");
                 number = rng.Next(1, 21);
                 guesses = 0;
             }
             else if (guess > number)
             {
-                MessageBox.Show("Du gissade för högt!");
+                MessageBox.Show("You guessed to low!");
             }
             else 
             {
-                MessageBox.Show("Du gissade för lågt!");
+                MessageBox.Show("You guessed to high!"); 
             }
+            //As you press the button that says "Guess", you guess is stored in the variable "guess". Your guess count also increases
+            //by one. Then, if you guessed correctly, you will be greeted with a box that says that you guessed right and after how many tries.
+            //If you guess too low or high, you will be greeted by a corresponing box.
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            number = rng.Next(1, 21);
+            number = rng.Next(1, 21); //As the project loads, the number is set to a value between 1 and 20. 
         }
     }
 }
